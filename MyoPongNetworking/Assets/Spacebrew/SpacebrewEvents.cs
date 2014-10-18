3283d78364695be2c1364c22e4984ce7d288bb3a
Lowell Bander <lowellbander@gmail.com>
2014-10-18 09:23:00 -0700

    added (possibly) placeholder variables

--- MyoPongNetworking/Assets/Spacebrew/SpacebrewEvents.cs
+++ MyoPongNetworking/Assets/Spacebrew/SpacebrewEvents.cs
@@ -10,6 +10,24 @@ public class SpacebrewEvents : MonoBehaviour {
 		GameObject go = GameObject.Find ("SpacebrewObject"); // the name of your client object
 		sbClient = go.GetComponent <SpacebrewClient> ();
 
+		// variables for reference.
+		// In production, these will likely be replaced with GameObject.Find("myObject").myAttribute.ToString();
+
+		// player1 is master, player2 is slave
+		bool isPlayer1 = true;
+
+		bool paddleOn1;
+		bool paddleOn2;
+
+		Vector3 paddlePos1;
+		Vector3 paddlePos2;
+
+		// below can later be extended to 
+		// Vector3[] ballPos1;
+		// Vector3[] ballPos2;
+
+		Vector3 ballPos1;
+		Vector3 ballPos2;
 	}
 
 	// Update is called once per frame
