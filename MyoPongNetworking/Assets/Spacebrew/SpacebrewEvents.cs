0bef3217f0ae4b004fa5cb4433e6f06df420b9b5
Lowell Bander <lowellbander@gmail.com>
2014-10-18 09:10:00 -0700

    about to start logic

--- MyoPongNetworking/Assets/Spacebrew/SpacebrewEvents.cs
+++ MyoPongNetworking/Assets/Spacebrew/SpacebrewEvents.cs
@@ -12,22 +12,22 @@ public class SpacebrewEvents : MonoBehaviour {
 
 		// register an event with the client and a callback function here.
 		// COMMON GOTCHA: THIS MUST MATCH THE NAME VALUE YOU TYPED IN THE EDITOR!!
-		sbClient.addEventListener (this.gameObject, "mystring");
+//		sbClient.addEventListener (this.gameObject, "mystring");
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown ("space")) {
-			print ("Sending Spacebrew Message");
+//		if (Input.GetKeyDown ("space")) {
+//			print ("Sending Spacebrew Message");
 			// name, type, value
 			// COMMON GOTCHA: THIS MUST MATCH THE NAME VALUE YOU TYPED IN THE EDITOR!!
-			sbClient.sendMessage("mybool", "boolean", "true");
+//			sbClient.sendMessage("mybool", "boolean", "true");
 		}
 	}
 
 	public void OnSpacebrewEvent(SpacebrewClient.SpacebrewMessage _msg) {
-		print ("Received Spacebrew Message");
-		print (_msg.value);
+//		print ("Received Spacebrew Message");
+//		print (_msg.value);
 	}
 
 }
