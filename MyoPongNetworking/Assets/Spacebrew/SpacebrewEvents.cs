df92eb37b2e5b684e3239973192f0f1d7c4c3833
Lowell Bander <lowellbander@gmail.com>
2014-10-18 09:28:38 -0700

    moved reference variables outside of Start()

--- MyoPongNetworking/Assets/Spacebrew/SpacebrewEvents.cs
+++ MyoPongNetworking/Assets/Spacebrew/SpacebrewEvents.cs
@@ -5,29 +5,29 @@ public class SpacebrewEvents : MonoBehaviour {
 
 	SpacebrewClient sbClient;
 
+	// VARIABLES FOR REFERENCE.
+	// In production, these will likely be replaced with GameObject.Find("myObject").myAttribute.ToString();
+	
+	// player1 is master, player2 is slave
+	bool isPlayer1 = true;
+	
+	bool paddleOn1;
+	bool paddleOn2;
+	
+	Vector3 paddlePos1;
+	Vector3 paddlePos2;
+	
+	// below can later be extended to 
+	// Vector3[] ballPos1;
+	// Vector3[] ballPos2;
+	
+	Vector3 ballPos1;
+	Vector3 ballPos2;
+
 	// Use this for initialization
 	void Start () {
 		GameObject go = GameObject.Find ("SpacebrewObject"); // the name of your client object
 		sbClient = go.GetComponent <SpacebrewClient> ();
-
-		// VARIABLES FOR REFERENCE.
-		// In production, these will likely be replaced with GameObject.Find("myObject").myAttribute.ToString();
-
-		// player1 is master, player2 is slave
-		bool isPlayer1 = true;
-
-		bool paddleOn1;
-		bool paddleOn2;
-
-		Vector3 paddlePos1;
-		Vector3 paddlePos2;
-
-		// below can later be extended to 
-		// Vector3[] ballPos1;
-		// Vector3[] ballPos2;
-
-		Vector3 ballPos1;
-		Vector3 ballPos2;
 
 		// REGISTER EVENT LISTENERS
 
